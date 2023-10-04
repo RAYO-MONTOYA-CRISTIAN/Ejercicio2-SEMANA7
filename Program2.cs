91bf51885195f5df63682cb1df5eba3357d8e705
RayoCristian <143460474+RAYO-MONTOYA-CRISTIAN@users.noreply.github.com>
2023-10-03 22:43:03 -0500

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i= 3; i <= 1000; i+= 3) 
+            {
+                int contador = i / 3;
+                Console.WriteLine($"Divisor de Omar Orlandini {contador}: {i}");
+
+            }
+        }
+    }
+}
